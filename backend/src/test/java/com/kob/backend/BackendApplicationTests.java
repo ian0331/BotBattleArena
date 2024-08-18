335baf2f105999af9844da4cccb54505c037b8c1
ian0331 <2062902667@qq.com>
2024-08-18 17:07:36 +0800

    后端数据库的配置

--- backend/src/test/java/com/kob/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/kob/backend/BackendApplicationTests.java
@@ -2,12 +2,19 @@ package com.kob.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.net.PasswordAuthentication;
 
 @SpringBootTest
 class BackendApplicationTests {
 
     @Test
     void contextLoads() {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        System.out.println(passwordEncoder.encode("ian0331"));
+        System.out.println(passwordEncoder.encode("fuyu0331"));
     }
 
 }
