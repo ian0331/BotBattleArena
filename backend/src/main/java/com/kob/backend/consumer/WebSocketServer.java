f618e75f25863be7688c9b66ee33ff5da8c508f1
ian0331 <2062902667@qq.com>
2024-09-10 19:39:45 +0800

    实现匹配系统（中），完成了对局记录的保存

--- backend/src/main/java/com/kob/backend/consumer/WebSocketServer.java
+++ backend/src/main/java/com/kob/backend/consumer/WebSocketServer.java
@@ -4,7 +4,9 @@ package com.kob.backend.consumer;
 import com.alibaba.fastjson.JSONObject;
 import com.kob.backend.consumer.utils.Game;
 import com.kob.backend.consumer.utils.JwtAuthentication;
+import com.kob.backend.mapper.RecordMapper;
 import com.kob.backend.mapper.UserMapper;
+import com.kob.backend.pojo.Record;
 import com.kob.backend.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -22,18 +24,22 @@ import java.util.concurrent.CopyOnWriteArraySet;
 @ServerEndpoint("/websocket/{token}")//注意不要以'/'结尾
 public class WebSocketServer {
 
-    final private static ConcurrentHashMap<Integer, WebSocketServer> users = new ConcurrentHashMap<>();
+    final public static ConcurrentHashMap<Integer, WebSocketServer> users = new ConcurrentHashMap<>();
     final private static CopyOnWriteArraySet<User> matchpool = new CopyOnWriteArraySet<>();
     private User user;
     private Session session=null;
 
     private static UserMapper userMapper;
-
+    public static RecordMapper recordMapper;
+    private Game game = null;
     @Autowired
     public void setUserMapper(UserMapper userMapper) {
         WebSocketServer.userMapper = userMapper;
     }
-
+    @Autowired
+    public void setRecordMapper(RecordMapper recordMapper) {
+        WebSocketServer.recordMapper = recordMapper;
+    }
     @OnOpen
     public void onOpen(Session session, @PathParam("token") String token) throws IOException {
         this.session = session;
@@ -68,22 +74,37 @@ public class WebSocketServer {
             matchpool.remove(a);
             matchpool.remove(b);
 
-            Game game = new Game(13,14,20);
+            Game game = new Game(13,14,20,a.getId(),b.getId());
             game.createMap();
+            game.start();
 
+
+            users.get(a.getId()).game = game;
+            users.get(b.getId()).game = game;
+
+
+
+            JSONObject respGame = new JSONObject();
+            respGame.put("a_id",game.getPlayerA().getId());
+            respGame.put("a_sx",game.getPlayerA().getSx());
+            respGame.put("a_sy",game.getPlayerA().getSy());
+            respGame.put("b_id",game.getPlayerB().getId());
+            respGame.put("b_sx",game.getPlayerB().getSx());
+            respGame.put("b_sy",game.getPlayerB().getSy());
+            respGame.put("map", game.getG());
 
             JSONObject respA = new JSONObject();
             respA.put("event", "start-matching");
             respA.put("opponent_username", b.getUsername());
             respA.put("opponent_photo", b.getPhoto());
-            respA.put("gamemap", game.getG());
+            respA.put("game", respGame);
             users.get(a.getId()).sendMessage(respA.toJSONString());
 
             JSONObject respB = new JSONObject();
             respB.put("event", "start-matching");
             respB.put("opponent_username", a.getUsername());
             respB.put("opponent_photo", a.getPhoto());
-            respB.put("gamemap", game.getG());
+            respB.put("game", respGame);
             users.get(b.getId()).sendMessage(respB.toJSONString());
 
         }
@@ -92,6 +113,15 @@ public class WebSocketServer {
         System.out.println("stopMatching");
         matchpool.remove(this.user);
     }
+
+    private void move(int direction){
+        if(game.getPlayerA().getId().equals(user.getId())){
+            game.setNextStepA(direction);
+        }else if(game.getPlayerB().getId().equals(user.getId())){
+            game.setNextStepB(direction);
+        }
+    }
+
 
     @OnMessage
     public void onMessage(String message, Session session) {//当作路由
@@ -102,6 +132,10 @@ public class WebSocketServer {
             startMatching();
         }else if("stop-matching".equals(event)) {
             stopMatching();
+        }else if("move".equals(event)) {
+            Integer direction = data.getInteger("direction");
+            System.out.println(direction);
+            move(direction);
         }
         // 从Client接收消息
     }
--- web/src/views/pk/PkIndexView.vue
+++ web/src/views/pk/PkIndexView.vue
@@ -1,18 +1,21 @@
 <template>
     <PlayGround v-if ="$store.state.pk.status==='playing'"></PlayGround>
     <MatchGround v-if ="$store.state.pk.status==='matching'"></MatchGround>
+    <ResultBoard v-if ="$store.state.pk.loser!=='none'"></ResultBoard>
 </template>
 
 <script>
 import PlayGround from "@/components/PlayGround.vue";
 import MatchGround from "@/components/MatchGround.vue";
+import ResultBoard from "@/components/ResultBoard.vue";
 import { onMounted, onUnmounted } from "vue";
 import { useStore } from "vuex";
 export default {
 
     components: {
         PlayGround,
-        MatchGround
+        MatchGround,
+        ResultBoard
     },
     setup() {
         const store = useStore();
@@ -39,7 +42,24 @@ export default {
                     setTimeout(()=>{
                         store.commit("updateStatus","playing");
                     },2000);
-                    store.commit("updateGamemap",data.gamemap);
+                    store.commit("updateGamemap",data.game);
+                }else if(data.event==="move"){
+                    console.log(data);
+                    const game = store.state.pk.gameObject;
+                    const [snake0,snake1] = game.snakes;
+                    snake0.set_direction(data.a_direction);
+                    snake1.set_direction(data.b_direction);
+                }else if(data.event==="result"){
+                    console.log(data);
+                    const game = store.state.pk.gameObject;
+                    const [snake0,snake1] = game.snakes;
+                    if(data.loser==="all"||data.loser==="A"){
+                        snake0.status = "die";
+                    }
+                    if(data.loser==="all"||data.loser==="B"){
+                        snake1.status = "die";
+                    }
+                    store.commit("updateLoser",data.loser);
                 }
             }
 
